278fb7962131bdbef1485750fa06d97644bace73
uruta <waltergagate443@gmail.com>
2024-12-31 17:45:23 +0800

    chore(image_path): Updated image paths 8

--- projects.js
+++ projects.js
@@ -3,91 +3,91 @@ const projects = [
         "id": 1,
         "name": "morels logo mockup",
         "category": "mockup",
-        "image": "public/images/1.jpg"
+        "image": "images/1.jpg"
     },
     {
         "id": 2,
         "name": "undefined logo",
         "category": "logo",
-        "image": "public/images/2.jpg"
+        "image": "images/2.jpg"
     },
     {
         "id": 3,
         "name": "padayon bogo mockup",
         "category": "mockup",
-        "image": "public/images/3.png"
+        "image": "images/3.png"
     },
     {
         "id": 4,
         "name": "together our_bubble defies gravity mockup",
         "category": "mockup",
-        "image": "public/images/4.png"
+        "image": "images/4.png"
     },
     {
         "id": 5,
         "name": "nalinaw ko na bang gusto ko sakin ka lang",
         "category": "poster",
-        "image": "public/images/5.png"
+        "image": "images/5.png"
     },
     {
         "id": 6,
         "name": "san man maglayag ikaw lang patutunguhan",
         "category": "poster",
-        "image": "public/images/6.png"
+        "image": "images/6.png"
     },
     {
         "id": 7,
         "name": "shit happens",
         "category": "poster",
-        "image": "public/images/7.png"
+        "image": "images/7.png"
     },
     {
         "id": 8,
         "name": "together our bubble defies gravity",
         "category": "poster",
-        "image": "public/images/8.png"
+        "image": "images/8.png"
     },
     {
         "id": 9,
         "name": "padayon bogo light",
         "category": "poster",
-        "image": "public/images/9.png"
+        "image": "images/9.png"
     },
     {
         "id": 10,
         "name": "padayon bogo dark",
         "category": "poster",
-        "image": "public/images/10.png"
+        "image": "images/10.png"
     },
     {
         "id": 11,
         "name": "shit happens mockup",
         "category": "mockup",
-        "image": "public/images/11.png"
+        "image": "images/11.png"
     },
     {
         "id": 12,
         "name": "comet",
         "category": "logo",
-        "image": "public/images/12.jpg"
+        "image": "images/12.jpg"
     },
     {
         "id": 13,
         "name": "lift",
         "category": "logo",
-        "image": "public/images/13.jpg"
+        "image": "images/13.jpg"
     },
     {
         "id": 14,
         "name": "floral prints",
         "category": "e-commerce",
-        "image": "public/images/14.png"
+        "image": "images/14.png"
     },
     {
         "id": 15,
         "name": "floral prints polo",
         "category": "e-commerce",
-        "image": "public/images/15.png"
+        "image": "images/15.png"
     }
 ]
 
